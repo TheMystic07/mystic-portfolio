81c8a7612f4675a0f8e1fbc78e6fb550b104bfa5
TheMystic07 <gurshabad90@gmail.com>
2024-05-12 07:24:51 +0530

    Updated Projects &  Added Live Links

--- lib/data.ts
+++ lib/data.ts
@@ -4,7 +4,10 @@ import { FaReact } from "react-icons/fa";
 import { LuGraduationCap } from "react-icons/lu";
 import corpcommentImg from "@/public/corpcomment.png";
 import ICPimg from "@/public/ICP2.png";
-import rmtdevImg from "@/public/rmtdev.png";
+import fps from "@/public/fps.png";
+import avengers from "@/public/avengers.png";
+import vision from "@/public/vision.png";
+import fit from "@/public/fit.png";
 import wordanalyticsImg from "@/public/wordanalytics.png";
 
 export const links = [
@@ -36,30 +39,13 @@ export const links = [
 
 export const experiencesData = [
   {
-    title: "Completed Diploma in Computer Science",
-    location: "R and D Polytechnic, Punjab, India",
+    title: "Chapter President",
+    location: "Geekroom Community, Chandigarh University, Punjab, India",
     description:
-      "I passed my diploma in computer science . I learned the basics of programming, data structures and algorithms , web dev with php and databases.",
-    icon: React.createElement(LuGraduationCap),
-    date: "2021-2023",
-  },
-  {
-    title: "Web Dev Trainee",
-    location: "Sandvok Solutions, Ludhiana, India",
-    description:
-      "Joined as a web development trainee. I worked on a few projects and learned about React,  Express and Frelancing.",
+      "Was selected as the chapter president of Geekroom for my University where i oraganized events and workshops .",
     icon: React.createElement(CgWorkAlt),
-    date: "AUG 2022 - AUG 2022",
+    date: "MAR 2024 - Present",
   },
-  {
-    title: "B.E   in Computer Science",
-    location: "Chandigarh University, Punjab, India",
-    description:
-      "Currently Studying Computer Science Engineering at Chandigarh University. I'm learning web development, data structures and algorithms, databases, operating systems and computer networks. I'm also a part of the university's coding club.",
-    icon: React.createElement(LuGraduationCap),
-    date: "2023 - 2026",
-  },
-
   {
     title: "Leader / Founder",
     location: "ARCHers, Chandigarh University, Punjab, India",
@@ -69,52 +55,76 @@ export const experiencesData = [
     date: "MAR 2024 - Present",
   },
   {
-    title: "Chapter President",
-    location: "Geekroom Community, Chandigarh University, Punjab, India",
+    title: "B.E in Computer Science",
+    location: "Chandigarh University, Punjab, India",
     description:
-      "Was selected as the chapter president of Geekroom for  my University where i   oraganzed events  and workshops .",
-    icon: React.createElement(CgWorkAlt),
-    date: "MAR 2024 - Present",
+      "Currently Studying Computer Science Engineering at Chandigarh University. I'm learning web development, data structures and algorithms, databases, operating systems and computer networks. I'm also a part of the university's coding club.",
+    icon: React.createElement(LuGraduationCap),
+    date: "2023 - 2026",
   },
-
+  {
+    title: "Web Dev Trainee",
+    location: "Sandvok Solutions, Ludhiana, India",
+    description:
+      "Joined as a web development trainee. I worked on a few projects and learned about React, Express and Frelancing.",
+    icon: React.createElement(CgWorkAlt),
+    date: "AUG 2022 - AUG 2022",
+  },
+  {
+    title: "Completed Diploma in Computer Science",
+    location: "R and D Polytechnic, Punjab, India",
+    description:
+      "I passed my diploma in computer science . I learned the basics of programming, data structures and algorithms , web dev with php and databases.",
+    icon: React.createElement(LuGraduationCap),
+    date: "2021-2023",
+  },
 ] as const;
 
 export const projectsData = [
   {
-    title: "ICP Kart",
+    title: "ICP Buggy ",
     description:
-      "A Go Kart racing game built with Unity.  Deployed on ICP mainet . ",
+      "A Go Kart racing game with AI self Driving  built with Unity.  Deployed on ICP mainet . ",
     tags: ["Unity", "WASM", "C#", "ICP", "Motoko"],
     imageUrl: ICPimg,
-    githubUrl: "",
-    demoUrl: "",
+    githubUrl: "https://github.com/TheMystic07/ICPBuggy",
+    demoUrl: "https://azirs-saaaa-aaaak-qif4q-cai.icp0.io/",
+  },
+  {
+    title: "ICP Shooter",
+    description:
+      "A FPS Shooter  game built with Unity Micogame.  Deployed on ICP mainet . ",
+    tags: ["Unity", "WASM", "C#", "ICP", "Motoko"],
+    imageUrl: fps,
+    githubUrl: "https://github.com/TheMystic07/ICPShooter",
+    demoUrl: "https://axk42-jqaaa-aaaak-qif5q-cai.icp0.io/",
   },
   {
     title: "ARCHers Assemble",
     description:
-      "A 3D Open world  Avengers game built with Unity. Where you can play as any Marvle Character and  explore the city , it is also deployes on ICP mainet . and it also had a NFT marketplace, where characters can be bought and sold.", 
-    tags: ["ICP", "Motoko", "Unity", "HTML", "CSS"],
-    imageUrl: rmtdevImg,
-    githubUrl: "",
-    demoUrl: "",
+      "A 3D Open world  Avengers game built with Unity. Where you can play as any Marvle Character and  explore the city , it is also deployes on Itch.io.", 
+    tags: [ "Unity","C#", "HTML", "CSS"],
+    imageUrl: avengers,
+    githubUrl: "https://github.com/TheMystic07/",
+    demoUrl: "https://mysticog.itch.io/archers-assemble",
   },
   {
     title: "ARCHers Vision",
     description:
-      "A Clone of Apple Vision Pro made in Unity with Mixed Reality  capabilities  at a  0.1% of the cost . We  Purchased an old  cheap vrbox and   made a hole for the camera.", 
-    tags: ["Mediapipe ", "Unity", "Android", "C#"],
-    imageUrl: rmtdevImg,
-    githubUrl: "",
-    demoUrl: "",
+      "A Clone of Apple Vision Pro made in Unity with Mixed Reality  capabilities  at a  0.1% of the cost . We  Used  an old  cheap vrbox .", 
+    tags: ["ManoMotion ", "Unity", "Android", "C#"],
+    imageUrl: vision,
+    githubUrl: "https://github.com/ARChers001/ARCHers_Vision/",
+    demoUrl: "https://anukuls-stellar-site.webflow.io/",
   },
   {
     title: "Form Forge ",
     description:
-      "An AI fitness trainer .", 
-    tags: ["ICP", "Motoko", "Unity", "HTML", "CSS"],
-    imageUrl: rmtdevImg,
+      "An AI fitness trainer  .", 
+    tags: ["Mediapipe", "Python", "Unity", "C#",],
+    imageUrl: fit,
     githubUrl: "",
-    demoUrl: "",
+    demoUrl: "https://formforge-sooty.vercel.app/",
   },
 
 ] as const;
@@ -129,10 +139,6 @@ export const skillsData = [
   "Node.js",
   "Git",
   "Tailwind",
-  "Prisma",
-  "MongoDB",
-  "Redux",
-  "GraphQL",
   "Apollo",
   "Express",
   "PostgreSQL",
@@ -143,11 +149,6 @@ export const skillsData = [
   "Flutter",
   "Blockchain",
   "Solidity",
-  "Web3.js",
-  "Truffle",
-  "Hardhat",
-  "Ethers.js",
-  "React Native",
   "Firebase",
   "Docker",
   "Kubernetes",
@@ -167,3 +168,4 @@ export const skillsData = [
   "Dart",
 
 ] as const;
+
